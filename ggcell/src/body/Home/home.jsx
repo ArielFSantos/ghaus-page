75bd0731e9f2574e9458cb9f83dedc9b3dd4e5aa
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-08-08 10:10:00 -0300

    att

--- ggcell/src/body/Home/home.jsx
+++ ggcell/src/body/Home/home.jsx
@@ -7,11 +7,7 @@ function Home(){
                 <h1>
                     <span>Frete </span> 
                     <span>Grátis </span> 
-                    Goiania e Região</h1>
-                    <h1 className="text-3xl font-bold underline">
-                        Hello world!
-                    </h1>
-                    
+                    Goiania e Região</h1>                    
             </div>
         </>
     )
