ce38c33c45f442bb9c7b0df4a9789623f3c3344e
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-08-20 20:28:29 -0300

    insert prints
    inserido prints para teste social

--- ggcell/src/body/social/social.tsx
+++ ggcell/src/body/social/social.tsx
@@ -1,7 +1,21 @@
-function social(){
+import React from "react";
+
+function Social ()  {
   return(
-    <>
-    <h1>teste</h1></>
+    <div className="h-screen w-full">
+      <h1 className="text-center text-5xl font-bold text-gray-500 pt-8 pb-10" >Masonry Layout</h1>
+      <div className="columns-4 gap-3 w-[1200px] mx-auto space-y-3 pb-28">
+        <div className="bg-gray-200 break-inside-avoid"><img src="https://pixabay.com/pt/illustrations/smartphone-telefone-ligar-m%C3%B3vel-4103051/" alt="whatever"/></div>
+        <div className="bg-gray-200 break-inside-avoid"><img src="https://pixabay.com/pt/illustrations/smartphone-telefone-ligar-m%C3%B3vel-4103051/" alt="whatever"/></div>
+        <div className="bg-gray-200 break-inside-avoid"><img src="https://pixabay.com/pt/illustrations/smartphone-telefone-ligar-m%C3%B3vel-4103051/" alt="whatever"/></div>
+        <div className="bg-gray-200 break-inside-avoid"><img src="https://pixabay.com/pt/illustrations/smartphone-telefone-ligar-m%C3%B3vel-4103051/" alt="whatever"/></div>
+        <div className="bg-gray-200 break-inside-avoid"><img src="https://pixabay.com/pt/illustrations/smartphone-telefone-ligar-m%C3%B3vel-4103051/" alt="whatever"/></div>
+        <div className="bg-gray-200 break-inside-avoid"><img src="https://pixabay.com/pt/illustrations/smartphone-telefone-ligar-m%C3%B3vel-4103051/" alt="whatever"/></div>
+      </div>
+
+
+
+    </div>
   )
 }
-export default social;
+export default Social;
