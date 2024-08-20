4fd90a9c5aad5a0d4e85b66df314496248f58c7a
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-08-20 20:33:45 -0300

    rename

--- ggcell/src/body/social/social.tsx
+++ ggcell/src/body/social/social.tsx
@@ -1,21 +1,39 @@
 import React from "react";
 
-function Social ()  {
-  return(
+// Importe suas imagens com o caminho correto
+import img1 from '../../assets/image-social/image1.jpg'; // Substitua pela extensão correta se necessário
+import img2 from '../../assets/image-social/image2.jpg';
+import img3 from '../../assets/image-social/image3.jpg';
+import img4 from '../../assets/image-social/image4.jpg';
+import img5 from '../../assets/image-social/image5.jpg';
+import img6 from '../../assets/image-social/image6.jpg';
+
+function Social() {
+  return (
     <div className="h-screen w-full">
-      <h1 className="text-center text-5xl font-bold text-gray-500 pt-8 pb-10" >Masonry Layout</h1>
+      <h1 className="text-center text-5xl font-bold text-gray-500 pt-8 pb-10">Masonry Layout</h1>
       <div className="columns-4 gap-3 w-[1200px] mx-auto space-y-3 pb-28">
-        <div className="bg-gray-200 break-inside-avoid"><img src="https://pixabay.com/pt/illustrations/smartphone-telefone-ligar-m%C3%B3vel-4103051/" alt="whatever"/></div>
-        <div className="bg-gray-200 break-inside-avoid"><img src="https://pixabay.com/pt/illustrations/smartphone-telefone-ligar-m%C3%B3vel-4103051/" alt="whatever"/></div>
-        <div className="bg-gray-200 break-inside-avoid"><img src="https://pixabay.com/pt/illustrations/smartphone-telefone-ligar-m%C3%B3vel-4103051/" alt="whatever"/></div>
-        <div className="bg-gray-200 break-inside-avoid"><img src="https://pixabay.com/pt/illustrations/smartphone-telefone-ligar-m%C3%B3vel-4103051/" alt="whatever"/></div>
-        <div className="bg-gray-200 break-inside-avoid"><img src="https://pixabay.com/pt/illustrations/smartphone-telefone-ligar-m%C3%B3vel-4103051/" alt="whatever"/></div>
-        <div className="bg-gray-200 break-inside-avoid"><img src="https://pixabay.com/pt/illustrations/smartphone-telefone-ligar-m%C3%B3vel-4103051/" alt="whatever"/></div>
+        <div className="bg-gray-200 break-inside-avoid">
+          <img src={img1} alt="Descrição da imagem 1" className="w-full h-auto" />
+        </div>
+        <div className="bg-gray-200 break-inside-avoid">
+          <img src={img2} alt="Descrição da imagem 2" className="w-full h-auto" />
+        </div>
+        <div className="bg-gray-200 break-inside-avoid">
+          <img src={img3} alt="Descrição da imagem 3" className="w-full h-auto" />
+        </div>
+        <div className="bg-gray-200 break-inside-avoid">
+          <img src={img4} alt="Descrição da imagem 4" className="w-full h-auto" />
+        </div>
+        <div className="bg-gray-200 break-inside-avoid">
+          <img src={img5} alt="Descrição da imagem 5" className="w-full h-auto" />
+        </div>
+        <div className="bg-gray-200 break-inside-avoid">
+          <img src={img6} alt="Descrição da imagem 6" className="w-full h-auto" />
+        </div>
       </div>
-
-
-
     </div>
-  )
+  );
 }
+
 export default Social;
