85199875ab63f5dbf66e2e4acebe1225693e6ce7
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-08-21 18:48:50 -0300

    att

--- ggcell/src/body/products/products.jsx
+++ ggcell/src/body/products/products.jsx
@@ -27,14 +27,12 @@ function Products() {
         breakpoint: 1024,
         settings: {
           slidesToShow: 4,
-          slidesToScroll: 1,
         },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 1,
         },
       },
     ],
